81a0ba2e264b114b2f65d2d3301b82e336b8db66
Artur <a.halilov@itcodegroup.ru>
2023-03-18 12:09:00 +0500

    fix

--- Edu112/asgi.py
+++ Edu112/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for savelife project.
+ASGI config for Edu112 project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'savelife.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Edu112.settings')
 
 application = get_asgi_application()
