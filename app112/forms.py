41fedcceee3adb19d036da31246e431d2dc4e804
Artur <halilov.artur@gmail.com>
2023-12-18 18:13:34 +0500

    Рефакторинг forms.py

--- app112/forms.py
+++ app112/forms.py
@@ -1,40 +1,43 @@
-from datetime import datetime
 from django import forms
 from django.core.exceptions import ValidationError
 from django.utils import timezone
 
-from .models import *
+from app112 import models
 
 
-class FormApplicant(forms.ModelForm):  # CreateApplicant
+class FormApplicant(forms.ModelForm):
     class Meta:
-        model = Applicant
+        model = models.Applicant
         fields = '__all__'
         widgets = {
-            'descr_state_health': forms.Textarea(attrs={"cols":"55", "rows":"5"})
+            'descr_state_health': forms.Textarea(attrs={'cols': '55', 'rows': '5'})
         }
 
     def clean_birthday(self):
-        birthday = self.cleaned_data['birthday']
+        birthday = self.cleaned_data.get('birthday')
+
         if birthday > timezone.now():
-            raise ValidationError("Дата рождения не должна быть в будущем времени!")
+            raise ValidationError('Дата рождения не должна быть в будущем времени!')
         return birthday
 
     def clean_phone(self):
-        phone = self.cleaned_data['phone']
+        phone = self.cleaned_data.get('phone')
+
         if len(str(phone)) > 11:
-            raise ValidationError("Номер телефона не должен превышать 11 цифр!")
+            raise ValidationError('Номер телефона не должен превышать 11 цифр!')
+        if len(str(phone)) < 6:
+            raise ValidationError('Номер телефона не должен быть меньше 6 цифр!')
         return phone
 
 
 class FormAppeal(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['applicant'].empty_label = "Не выбран"
-        self.fields['incidents'].empty_label = "Не выбран"
+        self.fields['applicant'].empty_label = 'Не выбран'
+        self.fields['incidents'].empty_label = 'Не выбран'
 
     class Meta:
-        model = Appeal
+        model = models.Appeal
         fields = '__all__'
         widgets = {
             'service': forms.CheckboxSelectMultiple(),
@@ -45,8 +48,8 @@ class FormAppeal(forms.ModelForm):
 class FormService(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['service_code'].help_text = "(Примечание: Код должен быть в виде двух цифр)"
+        self.fields['service_code'].help_text = 'Примечание: Код должен быть в виде двух цифр'
 
     class Meta:
-        model = EmergencyService
+        model = models.EmergencyService
         fields = '__all__'
