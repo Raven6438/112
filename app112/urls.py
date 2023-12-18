f99f6cd7ae3a6972b6ca9907d74e471855c90917
Artur <halilov.artur@gmail.com>
2023-12-18 17:43:06 +0500

    Рефакторинг urls.py

--- app112/urls.py
+++ app112/urls.py
@@ -1,9 +1,7 @@
-from django.urls import path, register_converter, reverse_lazy
+from django.urls import path, register_converter
 from django.views.generic import RedirectView
 
-from . import classviews
-from .classviews import Appeals, CreateApplicant, EditApplicant
-from .views import *
+from app112 import classviews, views
 
 
 class IDConverter:
@@ -23,21 +21,21 @@ urlpatterns = [
     # Отображение всех записей
     path('', RedirectView.as_view(url='/appeals/'), name='home'),
     path('applicants/', classviews.Applicants.as_view(), name='applicants'),
-    path('appeals/', Appeals.as_view(), name='appeals'),
+    path('appeals/', classviews.Appeals.as_view(), name='appeals'),
     # Создание записей
-    path('applicant/create/', CreateApplicant.as_view(), name='create_applicant'),
-    path('appeal/create/', create_appeal, name='create_appeal'),
-    path('service/create/', create_service, name='create_service'),
+    path('applicant/create/', classviews.CreateApplicant.as_view(), name='create_applicant'),
+    path('appeal/create/', views.create_appeal, name='create_appeal'),
+    path('service/create/', views.create_service, name='create_service'),
     # Редактирование записей
-    path('applicant/update/<int:pk>/', EditApplicant.as_view(), name='edit_applicant'),
-    path('appeal/update/<int:pk>/', edit_appeal, name='edit_appeal'),
-    path('service/update/<int:pk>/', edit_service, name='edit_service'),
+    path('applicant/update/<int:pk>/', classviews.EditApplicant.as_view(), name='edit_applicant'),
+    path('appeal/update/<int:pk>/', views.edit_appeal, name='edit_appeal'),
+    path('service/update/<int:pk>/', views.edit_service, name='edit_service'),
     # Отображение записей по ID
-    path('applicant/<int:pk>/', get_applicant, name='get_applicant'),
-    path('service/<int:pk>/', get_service, name='get_service'),
+    path('applicant/<int:pk>/', views.get_applicant, name='get_applicant'),
+    path('service/<int:pk>/', views.get_service, name='get_service'),
     # Отображение других данных
-    path('applicant_by_phone/', get_applicant_by_phone, name='get_applicant_by_phone'),
-    path('data_json/<int:pk>/', data_json, name='data_json'),
-    path('redirect/', redir, name='redirect'),
-    path('redirect2/', redir2, name='redirect2')
+    path('applicant_by_phone/', views.get_applicant_by_phone, name='get_applicant_by_phone'),
+    path('data_json/<int:pk>/', views.data_json, name='data_json'),
+    path('redirect/', views.redir, name='redirect'),
+    path('redirect2/', views.redir2, name='redirect2')
 ]
